0b5f598772b5c5a6a6bee2c4e5d7ae1f05e0f8c5
Toon Van den Bos <toon@whitecube.be>
2024-06-13 16:50:48 +0200

    Fixed seclib typehinting

--- src/SecLibGateway.php
+++ src/SecLibGateway.php
@@ -284,7 +284,7 @@ class SecLibGateway implements GatewayInterface
      */
     public function run($command)
     {
-        $this->getConnection()->exec($command, false);
+        $this->getConnection()->exec($command, null);
     }
 
     /**
